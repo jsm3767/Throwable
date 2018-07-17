8b740371390a134639ba0fdf8de826d996548236
Aria Myers <jsm3767@g.rit.edu>
2018-07-16 23:11:00 -0400

    Basic Mechanic Setup and Testing
    Got some preliminary classes in there to test the funcitonalit of a
    grenade class

---
+++ Throwable/Assets/Scripts/PulseGrenade.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulseGrenade : MonoBehaviour {
+
+	float radius = 15;
+	[SerializeField] float pushForce = 10;
+	[SerializeField] SphereCollider explosionRadius;
+	List<GameObject> objInExplosion;
+	[SerializeField] float fuseTime = 3;
+	float fuseProgress = 0;
+	bool pinPulled = false;
+
+	// Use this for initialization
+	void Start () {
+		objInExplosion = new List<GameObject> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (pinPulled) {
+			fuseProgress += Time.deltaTime;
+		}
+		if (fuseProgress >= fuseTime) {
+			Explode ();
+		}
+	}
+
+	public void PullPin () {
+		print ("Pin pulled");
+		pinPulled = true;
+	}
+
+	void Explode () {
+		print ("Boom");
+		foreach (GameObject go in objInExplosion) {
+			if (!go) {
+				break;
+			}
+			Rigidbody objRb = go.GetComponent<Rigidbody> ();
+			if (objRb) {
+				Vector3 fromGrenadeToObj = go.transform.position - this.gameObject.transform.position;
+				fromGrenadeToObj.Normalize ();
+				objRb.velocity += fromGrenadeToObj * pushForce;
+			}
+		}
+		Destroy (this.gameObject, 0.1f);
+	}
+
+	void OnTriggerEnter (Collider c) {
+		objInExplosion.Add (c.gameObject);
+	}
+
+	void OnTriggerExit (Collider c){
+		objInExplosion.Remove (c.gameObject);
+	}
+}
---
+++ Throwable/Assets/Scripts/Thrower.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thrower : MonoBehaviour {
+
+	[SerializeField] GameObject grenadePrefab;
+	[SerializeField] Camera camera;
+	[SerializeField] float throwStrength = 30;
+	bool fireReady = true;
+	private GameObject activeGrenade;
+
+
+	// Use this for initialization
+	void Start () {
+		camera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Fire1") == 1 && fireReady) {
+			GameObject grenadeSpawn = GameObject.Instantiate (grenadePrefab);
+			activeGrenade = grenadeSpawn;
+			activeGrenade.transform.position = camera.transform.position;
+			activeGrenade.transform.position += camera.transform.forward * 2;
+			activeGrenade.transform.forward = camera.transform.forward;
+			fireReady = false;
+			PulseGrenade pg = grenadeSpawn.GetComponent<PulseGrenade> ();
+			if (pg) {
+				pg.PullPin ();
+			}
+		} else if (Input.GetAxis ("Fire1") == 0 && !fireReady){
+			fireReady = true;
+			Rigidbody grenadeRB = activeGrenade.GetComponent<Rigidbody> ();
+			grenadeRB.velocity = activeGrenade.transform.forward * throwStrength;
+		} else if (Input.GetAxis ("Fire1") == 1 && !fireReady){
+			activeGrenade.transform.position = camera.transform.position;
+			activeGrenade.transform.position += camera.transform.forward * 2;
+			activeGrenade.transform.forward = camera.transform.forward;
+		}
+	}
+}
